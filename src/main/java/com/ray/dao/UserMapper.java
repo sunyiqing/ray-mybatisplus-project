4399d2b4cbfdc0505df2cc08541f220ca576c2b9
suneqing <suneqing@163.com>
2019-09-12 14:12:55 +0800

    mybatisplus第一次提交

---
+++ src/main/java/com/ray/dao/UserMapper.java
@@ -0,0 +1,8 @@
+package com.ray.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ray.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/ray/entity/User.java
@@ -0,0 +1,11 @@
+package com.ray.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
